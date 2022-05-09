8100db6739609df6d7cc36a860bbfc7f231a070e
Xiaofei Du <xiaofei.du@instill.tech>
2022-05-09 22:35:00 +0100

    fix: refactor check more type of fields

--- checkfield/checkfield.go
+++ checkfield/checkfield.go
@@ -10,6 +10,7 @@ import (
 )
 
 // CheckRequiredFields implements follows https://google.aip.dev/203#required
+// TODO limitation: can't handle number and pointer fields
 func CheckRequiredFields(msg interface{}, requiredFields []string) error {
 	for i := 0; i < reflect.Indirect(reflect.ValueOf(msg)).NumField(); i++ {
 		fieldName := reflect.Indirect(reflect.ValueOf(msg)).Type().Field(i).Name
@@ -18,15 +19,11 @@ func CheckRequiredFields(msg interface{}, requiredFields []string) error {
 			switch f.Kind() {
 			case reflect.String:
 				if f.String() == "" {
-					return status.Errorf(codes.InvalidArgument, "Required field %s is not provided", fieldName)
+					return status.Errorf(codes.InvalidArgument, "required field `%s` is not provided", fieldName)
 				}
 			case reflect.Ptr:
 				if f.IsNil() {
-					return status.Errorf(codes.InvalidArgument, "Required field %s is not provided", fieldName)
-				} else if reflect.Indirect(reflect.ValueOf(f)).Kind() == reflect.Struct {
-					if err := CheckRequiredFields(f.Interface(), requiredFields); err != nil {
-						return err
-					}
+					return status.Errorf(codes.InvalidArgument, "required field `%s` is not provided", fieldName)
 				}
 			}
 		}
@@ -35,34 +32,62 @@ func CheckRequiredFields(msg interface{}, requiredFields []string) error {
 }
 
 // CheckOutputOnlyFields implements follows https://google.aip.dev/203#output-only
+// TODO Limitation: can't handle struct
 func CheckOutputOnlyFields(msg interface{}, outputOnlyFields []string) error {
 	for _, field := range outputOnlyFields {
 		f := reflect.Indirect(reflect.ValueOf(msg)).FieldByName(field)
 		switch f.Kind() {
-		case reflect.Int32:
+		case reflect.Bool:
+			reflect.ValueOf(msg).Elem().FieldByName(field).SetBool(false)
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
 			reflect.ValueOf(msg).Elem().FieldByName(field).SetInt(0)
+		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+			reflect.ValueOf(msg).Elem().FieldByName(field).SetUint(uint64(0))
+		case reflect.Float32, reflect.Float64:
+			reflect.ValueOf(msg).Elem().FieldByName(field).SetFloat(0)
 		case reflect.String:
 			reflect.ValueOf(msg).Elem().FieldByName(field).SetString("")
 		case reflect.Ptr:
 			reflect.ValueOf(msg).Elem().FieldByName(field).Set(reflect.Zero(f.Type()))
-		case reflect.Struct:
-			if err := CheckOutputOnlyFields(f, outputOnlyFields); err != nil {
-				return err
-			}
 		}
 	}
 	return nil
 }
 
 // CheckImmutableFields implements follows https://google.aip.dev/203#immutable
-func CheckImmutableFields(msgReq *interface{}, msgUpdate *interface{}, immutableFields []string) error {
+// TODO Limitation: can't handle struct or pointer field
+func CheckImmutableFields(msgReq interface{}, msgUpdate interface{}, immutableFields []string) error {
 	for _, field := range immutableFields {
 		f := reflect.Indirect(reflect.ValueOf(msgReq)).FieldByName(field)
 		switch f.Kind() {
+		case reflect.Bool:
+			if f.Bool() {
+				if f.Bool() != reflect.Indirect(reflect.ValueOf(msgUpdate)).FieldByName(field).Bool() {
+					return status.Errorf(codes.InvalidArgument, "field `%s` is immutable", field)
+				}
+			}
 		case reflect.String:
 			if f.String() != "" {
 				if f.String() != reflect.Indirect(reflect.ValueOf(msgUpdate)).FieldByName(field).String() {
-					return status.Errorf(codes.InvalidArgument, "Field %s is immutable", field)
+					return status.Errorf(codes.InvalidArgument, "field `%s` is immutable", field)
+				}
+			}
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			if f.Int() != 0 {
+				if f.Int() != reflect.Indirect(reflect.ValueOf(msgUpdate)).FieldByName(field).Int() {
+					return status.Errorf(codes.InvalidArgument, "field `%v` is immutable", field)
+				}
+			}
+		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+			if f.Uint() != 0 {
+				if f.Uint() != reflect.Indirect(reflect.ValueOf(msgUpdate)).FieldByName(field).Uint() {
+					return status.Errorf(codes.InvalidArgument, "field `%v` is immutable", field)
+				}
+			}
+		case reflect.Float32, reflect.Float64:
+			if f.Float() != 0 {
+				if f.Float() != reflect.Indirect(reflect.ValueOf(msgUpdate)).FieldByName(field).Float() {
+					return status.Errorf(codes.InvalidArgument, "field `%v` is immutable", field)
 				}
 			}
 		}
@@ -72,13 +97,13 @@ func CheckImmutableFields(msgReq *interface{}, msgUpdate *interface{}, immutable
 
 // CheckResourceID implements follows https://google.aip.dev/122#resource-id-segments
 func CheckResourceID(id string) error {
-	if match, _ := regexp.MatchString("^[a-z]([a-z0-9-]{0,61}[a-z0-9])?$", id); !match {
-		return status.Error(codes.InvalidArgument, "The id of pipeline needs to be within ASCII-only 63 characters following RFC-1034 with a regexp (^[a-z]([a-z0-9-]{0,61}[a-z0-9])?$)")
-	}
 	if _, err := uuid.Parse(id); err == nil {
-		return status.Error(codes.InvalidArgument, "The id is a UUID, which is not allowed")
+		return status.Error(codes.InvalidArgument, "`id` is not allowed to be a UUID")
 	}
 
+	if match, _ := regexp.MatchString("^[a-z]([a-z0-9-]{0,61}[a-z0-9])?$", id); !match {
+		return status.Error(codes.InvalidArgument, "`id` needs to be within ASCII-only 63 characters following RFC-1034 with a regexp (^[a-z]([a-z0-9-]{0,61}[a-z0-9])?$)")
+	}
 	return nil
 }
 
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.17
 require (
 	github.com/gogo/status v1.1.0
 	github.com/google/uuid v1.3.0
+	github.com/stretchr/testify v1.7.0
 	go.temporal.io/sdk v1.13.1
 	go.uber.org/zap v1.21.0
 	google.golang.org/grpc v1.42.0
@@ -12,10 +13,13 @@ require (
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gogo/googleapis v1.4.1 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 	google.golang.org/genproto v0.0.0-20211104193956-4c6863e31247 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
