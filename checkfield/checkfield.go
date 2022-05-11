83b1d7b1bffd04b39bb007affc3c5beb1ade6ae0
Xiaofei Du <xiaofei.du@instill.tech>
2022-05-11 02:14:51 +0100

    fix: refactor checkfield functions and add tests (#3)
    Because
    
    - Test whether the functions works as expected
    
    This commit
    
    - refactor functions in `checkfield`
      - check more types of fields and limitations of the functions
    - add `checkfield` tests
    
    Co-authored-by: Ping-Lin Chang <ping-lin.chang@instill.tech>

--- checkfield/checkfield.go
+++ checkfield/checkfield.go
@@ -3,82 +3,210 @@ package checkfield
 import (
 	"reflect"
 	"regexp"
+	"strings"
 
 	"github.com/gogo/status"
 	"github.com/google/uuid"
+	"github.com/iancoleman/strcase"
 	"google.golang.org/grpc/codes"
+	"google.golang.org/protobuf/types/known/fieldmaskpb"
 )
 
-// CheckRequiredFields implements follows https://google.aip.dev/203#required
-func CheckRequiredFields(msg interface{}, requiredFields []string) error {
-	for i := 0; i < reflect.Indirect(reflect.ValueOf(msg)).NumField(); i++ {
-		fieldName := reflect.Indirect(reflect.ValueOf(msg)).Type().Field(i).Name
-		if contains(requiredFields, fieldName) {
-			f := reflect.Indirect(reflect.ValueOf(msg)).FieldByName(fieldName)
-			switch f.Kind() {
-			case reflect.String:
-				if f.String() == "" {
-					return status.Errorf(codes.InvalidArgument, "Required field %s is not provided", fieldName)
+// CheckRequiredFieldsCreate implements follows https://google.aip.dev/203#required
+// The msg parameter is a Protobuf message instance
+// The requiredFields is a slice of field path with snake_case name
+func CheckRequiredFieldsCreate(msg interface{}, requiredFields []string) error {
+
+	var recurMsgCheck func(interface{}, []string, string) error
+	recurMsgCheck = func(m interface{}, fieldNames []string, path string) error {
+		f := reflect.Indirect(reflect.ValueOf(m)).FieldByName(strcase.ToCamel(fieldNames[0]))
+		switch f.Kind() {
+		case reflect.Invalid:
+			return status.Errorf(codes.InvalidArgument, "required field path `%s` is not found in the Protobuf message", path)
+		case reflect.String:
+			if f.String() == "" {
+				return status.Errorf(codes.InvalidArgument, "required field path `%s` is not assigned", path)
+			}
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			if f.Int() == 0 {
+				return status.Errorf(codes.InvalidArgument, "required field path `%s` is not assigned", path)
+			}
+		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+			if f.Uint() == 0 {
+				return status.Errorf(codes.InvalidArgument, "required field path `%s` is not assigned", path)
+			}
+		case reflect.Float32, reflect.Float64:
+			if f.Float() == 0 {
+				return status.Errorf(codes.InvalidArgument, "required field path `%s` is not assigned", path)
+			}
+		case reflect.Struct:
+			if len(fieldNames) > 1 {
+				path, fieldNames = path+"."+fieldNames[0], fieldNames[1:]
+				if err := recurMsgCheck(f.Interface(), fieldNames, path); err != nil {
+					return err
 				}
-			case reflect.Ptr:
-				if f.IsNil() {
-					return status.Errorf(codes.InvalidArgument, "Required field %s is not provided", fieldName)
-				} else if reflect.Indirect(reflect.ValueOf(f)).Kind() == reflect.Struct {
-					if err := CheckRequiredFields(f.Interface(), requiredFields); err != nil {
+			}
+		case reflect.Ptr:
+			if f.IsNil() {
+				return status.Errorf(codes.InvalidArgument, "required field path `%s` is not assigned", path)
+			} else if reflect.ValueOf(f).Kind() == reflect.Struct {
+				if len(fieldNames) > 1 {
+					path, fieldNames = path+"."+fieldNames[0], fieldNames[1:]
+					if err := recurMsgCheck(f.Interface(), fieldNames, path); err != nil {
 						return err
 					}
 				}
 			}
 		}
+		return nil
 	}
-	return nil
-}
 
-// CheckOutputOnlyFields implements follows https://google.aip.dev/203#output-only
-func CheckOutputOnlyFields(msg interface{}, outputOnlyFields []string) error {
-	for _, field := range outputOnlyFields {
-		f := reflect.Indirect(reflect.ValueOf(msg)).FieldByName(field)
-		switch f.Kind() {
-		case reflect.Int32:
-			reflect.ValueOf(msg).Elem().FieldByName(field).SetInt(0)
-		case reflect.String:
-			reflect.ValueOf(msg).Elem().FieldByName(field).SetString("")
-		case reflect.Ptr:
-			reflect.ValueOf(msg).Elem().FieldByName(field).Set(reflect.Zero(f.Type()))
-		case reflect.Struct:
-			if err := CheckOutputOnlyFields(f, outputOnlyFields); err != nil {
-				return err
-			}
+	for _, path := range requiredFields {
+		fieldNames := strings.Split(path, ".")
+		if err := recurMsgCheck(msg, fieldNames, fieldNames[0]); err != nil {
+			return err
 		}
 	}
+
 	return nil
 }
 
-// CheckImmutableFields implements follows https://google.aip.dev/203#immutable
-func CheckImmutableFields(msgReq *interface{}, msgUpdate *interface{}, immutableFields []string) error {
-	for _, field := range immutableFields {
-		f := reflect.Indirect(reflect.ValueOf(msgReq)).FieldByName(field)
+// CheckOutputOnlyFieldsCreate implements follows https://google.aip.dev/203#output-only
+// The msg parameter is a Protobuf message instance
+// The outputOnlyFields is a slice of field path with snake_case name
+func CheckOutputOnlyFieldsCreate(msg interface{}, outputOnlyFields []string) error {
+
+	var recurMsgCheck func(interface{}, []string, string) error
+	recurMsgCheck = func(m interface{}, fieldNames []string, path string) error {
+		fieldName := strcase.ToCamel(fieldNames[0])
+		f := reflect.ValueOf(m).Elem().FieldByName(fieldName)
 		switch f.Kind() {
+		case reflect.Invalid:
+			return status.Errorf(codes.InvalidArgument, "output-only field path `%s` is not found in the Protobuf message", path)
+		case reflect.Bool:
+			f.SetBool(false)
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			f.SetInt(0)
+		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+			f.SetUint(uint64(0))
+		case reflect.Float32, reflect.Float64:
+			f.SetFloat(0)
 		case reflect.String:
-			if f.String() != "" {
-				if f.String() != reflect.Indirect(reflect.ValueOf(msgUpdate)).FieldByName(field).String() {
-					return status.Errorf(codes.InvalidArgument, "Field %s is immutable", field)
+			f.SetString("")
+		case reflect.Ptr:
+			if len(fieldNames) > 1 && reflect.ValueOf(f).Kind() == reflect.Struct {
+				path, fieldNames = path+"."+fieldNames[0], fieldNames[1:]
+				if err := recurMsgCheck(f.Interface(), fieldNames, path); err != nil {
+					return err
+				}
+			} else {
+				f.Set(reflect.Zero(f.Type()))
+			}
+		}
+		return nil
+	}
+
+	for _, path := range outputOnlyFields {
+		fieldNames := strings.Split(path, ".")
+		if err := recurMsgCheck(msg, fieldNames, fieldNames[0]); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// CheckImmutableFieldsUpdate implements follows https://google.aip.dev/203#immutable
+// The msgReq parameter is a Protobuf message instance requested to update msgUpdate
+// The outputOnlyFields is a slice of field path with snake_case name
+func CheckImmutableFieldsUpdate(msgReq interface{}, msgUpdate interface{}, immutableFields []string) error {
+
+	var recurMsgCheck func(interface{}, interface{}, []string, string) error
+	recurMsgCheck = func(mr interface{}, mu interface{}, fieldNames []string, path string) error {
+		fieldName := strcase.ToCamel(fieldNames[0])
+		f := reflect.Indirect(reflect.ValueOf(mr)).FieldByName(fieldName)
+		switch f.Kind() {
+		case reflect.Invalid:
+			return status.Errorf(codes.InvalidArgument, "immutable field path `%s` is not found in the Protobuf message", path)
+		case reflect.Bool:
+			if f.Bool() {
+				if f.Bool() != reflect.Indirect(reflect.ValueOf(mu)).FieldByName(fieldName).Bool() {
+					return status.Errorf(codes.InvalidArgument, "field path `%s` is immutable", path)
 				}
 			}
+		case reflect.String:
+			if f.String() != "" {
+				if f.String() != reflect.Indirect(reflect.ValueOf(mu)).FieldByName(fieldName).String() {
+					return status.Errorf(codes.InvalidArgument, "field path `%s` is immutable", path)
+				}
+			}
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			if f.Int() != 0 {
+				if f.Int() != reflect.Indirect(reflect.ValueOf(mu)).FieldByName(fieldName).Int() {
+					return status.Errorf(codes.InvalidArgument, "field path `%v` is immutable", path)
+				}
+			}
+		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+			if f.Uint() != 0 {
+				if f.Uint() != reflect.Indirect(reflect.ValueOf(mu)).FieldByName(fieldName).Uint() {
+					return status.Errorf(codes.InvalidArgument, "field path `%v` is immutable", path)
+				}
+			}
+		case reflect.Float32, reflect.Float64:
+			if f.Float() != 0 {
+				if f.Float() != reflect.Indirect(reflect.ValueOf(mu)).FieldByName(fieldName).Float() {
+					return status.Errorf(codes.InvalidArgument, "field path `%v` is immutable", path)
+				}
+			}
+		case reflect.Ptr:
+			if len(fieldNames) > 1 && reflect.ValueOf(f).Kind() == reflect.Struct {
+				path, fieldNames = path+"."+fieldNames[0], fieldNames[1:]
+				if err := recurMsgCheck(f.Interface(), reflect.Indirect(reflect.ValueOf(mu)).FieldByName(fieldName).Interface(), fieldNames, path); err != nil {
+					return err
+				}
+			} else {
+				f.Set(reflect.Zero(f.Type()))
+			}
+		}
+		return nil
+	}
+
+	for _, path := range immutableFields {
+		fieldNames := strings.Split(path, ".")
+		if err := recurMsgCheck(msgReq, msgUpdate, fieldNames, fieldNames[0]); err != nil {
+			return err
 		}
 	}
+
 	return nil
+}
+
+// CheckOutputOnlyFieldsUpdate removes output only fields from the input field mask
+//
+// output only fields are in `CamelCase` format and fields in the field mask are in `snake_case` format.
+// if a path in the input field mask is nested, such as `a.b`, and the output only fields includes `A`,
+// the path will be removed. The unfiltered paths will be stored in the the output field mask
+// in the original format.
+func CheckOutputOnlyFieldsUpdate(mask *fieldmaskpb.FieldMask, outputOnlyFields []string) (*fieldmaskpb.FieldMask, error) {
+	maskUpdated := new(fieldmaskpb.FieldMask)
+
+	for _, path := range mask.GetPaths() {
+		if !contains(outputOnlyFields, path) {
+			maskUpdated.Paths = append(maskUpdated.Paths, path)
+		}
+	}
+	return maskUpdated, nil
 }
 
 // CheckResourceID implements follows https://google.aip.dev/122#resource-id-segments
 func CheckResourceID(id string) error {
-	if match, _ := regexp.MatchString("^[a-z]([a-z0-9-]{0,61}[a-z0-9])?$", id); !match {
-		return status.Error(codes.InvalidArgument, "The id of pipeline needs to be within ASCII-only 63 characters following RFC-1034 with a regexp (^[a-z]([a-z0-9-]{0,61}[a-z0-9])?$)")
-	}
 	if _, err := uuid.Parse(id); err == nil {
-		return status.Error(codes.InvalidArgument, "The id is a UUID, which is not allowed")
+		return status.Error(codes.InvalidArgument, "`id` is not allowed to be a UUID")
 	}
 
+	if match, _ := regexp.MatchString("^[a-z]([a-z0-9-]{0,61}[a-z0-9])?$", id); !match {
+		return status.Error(codes.InvalidArgument, "`id` needs to be within ASCII-only 63 characters following RFC-1034 with a regexp (^[a-z]([a-z0-9-]{0,61}[a-z0-9])?$)")
+	}
 	return nil
 }
 
---
+++ checkfield/checkfield_test.go
@@ -0,0 +1,356 @@
+package checkfield_test
+
+import (
+	"testing"
+
+	"github.com/instill-ai/x/checkfield"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCheckRequiredFieldsCreate_NoError(t *testing.T) {
+
+	type A struct {
+		A struct {
+			B struct {
+				C string
+			}
+		}
+		D string
+	}
+
+	msg := new(A)
+	msg.A.B.C = "some nested field"
+	msg.D = "some nested field"
+
+	requiredFields := []string{"a.b.c", "d", "a.b", "a"}
+
+	err := checkfield.CheckRequiredFieldsCreate(msg, requiredFields)
+	require.NoError(t, err)
+
+}
+
+func TestCheckRequiredFieldsCreate_Error(t *testing.T) {
+
+	type A struct {
+		A struct {
+			B struct {
+				C string
+			}
+		}
+		D string
+	}
+
+	msg := new(A)
+
+	requiredFields := []string{"a.b.c", "d", "a.b", "a"}
+
+	err := checkfield.CheckRequiredFieldsCreate(msg, requiredFields)
+	require.Error(t, err)
+
+}
+
+func TestCheckRequiredFieldsCreate_RequiredString(t *testing.T) {
+	type A struct {
+		Field1 string
+	}
+	requiredFields := []string{"field1"}
+
+	msg := new(A)
+
+	err := checkfield.CheckRequiredFieldsCreate(msg, requiredFields)
+	require.EqualError(t, err, "rpc error: code = InvalidArgument desc = required field path `field1` is not assigned")
+}
+
+func TestCheckRequiredFieldsCreate_RequiredPtr(t *testing.T) {
+	type A struct {
+		Field1 *string
+	}
+	requiredFields := []string{"field1"}
+
+	msg := new(A)
+
+	err := checkfield.CheckRequiredFieldsCreate(msg, requiredFields)
+	require.EqualError(t, err, "rpc error: code = InvalidArgument desc = required field path `field1` is not assigned")
+}
+
+func TestCheckRequiredFieldsCreate_RequiredStructPtr(t *testing.T) {
+	type A struct {
+		Field1 string
+	}
+	type B struct {
+		Field2 *A
+	}
+	requiredFields := []string{"field2"}
+
+	msg := new(B)
+
+	err := checkfield.CheckRequiredFieldsCreate(msg, requiredFields)
+	require.EqualError(t, err, "rpc error: code = InvalidArgument desc = required field path `field2` is not assigned")
+}
+
+func TestCheckRequiredFieldsCreate_RequiredNestedValid(t *testing.T) {
+	type A struct {
+		Field1 string
+		Field2 string
+	}
+	type B struct {
+		Field1 *A
+	}
+	requiredFields := []string{"field1"}
+
+	msg := &B{
+		Field1: &A{
+			Field1: "field1_A",
+		},
+	}
+
+	err := checkfield.CheckRequiredFieldsCreate(msg, requiredFields)
+	require.NoError(t, err)
+}
+
+func TestCheckRequiredFieldsCreate_RequiredNestedInValid(t *testing.T) {
+	type A struct {
+		Field1 string
+		Field2 string
+	}
+	type B struct {
+		Field1 *A
+	}
+	requiredFields := []string{"field1.field1"}
+
+	msg := &B{
+		Field1: &A{
+			Field2: "field2_A",
+		},
+	}
+
+	err := checkfield.CheckRequiredFieldsCreate(msg, requiredFields)
+	require.EqualError(t, err, "rpc error: code = InvalidArgument desc = required field path `field1.field1` is not assigned")
+}
+
+func TestCheckOutputOnlyFieldsCreate_Valid(t *testing.T) {
+
+	type InnerStruct struct {
+		FieldStr string
+	}
+
+	type A struct {
+		FieldBool                bool
+		FieldInt                 int
+		FieldInt8                int8
+		FieldInt16               int16
+		FieldInt32               int32
+		FieldInt64               int64
+		FieldUint                uint
+		FieldUint8               uint8
+		FieldUint16              uint16
+		FieldUint32              uint32
+		FieldUint64              uint64
+		FieldFloat32             float32
+		FieldFloat64             float64
+		FieldStr                 string
+		FieldStrPtr              *string
+		FieldStrNotOutputOnly    string
+		FieldStrPtrNotOutputOnly *string
+		FieldNestedStruct        *InnerStruct
+	}
+	outputFields := []string{
+		"field_bool",
+		"field_int", "field_int8", "field_int16", "field_int32", "field_int64",
+		"field_uint", "field_uint8", "field_uint16", "field_uint32", "field_uint64",
+		"field_float32", "field_float64",
+		"field_str", "field_str_ptr",
+		"field_nested_struct.field_str",
+	}
+
+	nonEmptyStr := "field"
+	msg := &A{
+		FieldBool:                true,
+		FieldInt:                 10,
+		FieldInt8:                10,
+		FieldInt16:               10,
+		FieldInt32:               10,
+		FieldInt64:               10,
+		FieldUint:                10,
+		FieldUint8:               10,
+		FieldUint16:              10,
+		FieldUint32:              10,
+		FieldUint64:              10,
+		FieldFloat32:             10,
+		FieldFloat64:             10,
+		FieldStr:                 "field_str",
+		FieldStrPtr:              &nonEmptyStr,
+		FieldStrNotOutputOnly:    "field_str_not_output_only",
+		FieldStrPtrNotOutputOnly: &nonEmptyStr,
+		FieldNestedStruct: &InnerStruct{
+			FieldStr: nonEmptyStr,
+		},
+	}
+
+	err := checkfield.CheckOutputOnlyFieldsCreate(msg, outputFields)
+	require.NoError(t, err)
+	require.Equal(t, &A{
+		FieldBool:                false,
+		FieldInt:                 0,
+		FieldInt8:                0,
+		FieldInt16:               0,
+		FieldInt32:               0,
+		FieldInt64:               0,
+		FieldUint:                0,
+		FieldUint8:               0,
+		FieldUint16:              0,
+		FieldUint32:              0,
+		FieldUint64:              0,
+		FieldFloat32:             0,
+		FieldFloat64:             0,
+		FieldStr:                 "",
+		FieldStrPtr:              nil,
+		FieldStrPtrNotOutputOnly: &nonEmptyStr,
+		FieldStrNotOutputOnly:    "field_str_not_output_only",
+		FieldNestedStruct: &InnerStruct{
+			FieldStr: "",
+		},
+	}, msg)
+}
+
+func TestCheckImmutableFieldsUpdate_NoUpdate(t *testing.T) {
+	type A struct {
+		Field1 string
+	}
+	immutableFields := []string{"field1"}
+
+	msgReq := &A{
+		Field1: "msgUpdate",
+	}
+	msgUpdate := &A{
+		Field1: "msgUpdate",
+	}
+
+	err := checkfield.CheckImmutableFieldsUpdate(msgReq, msgUpdate, immutableFields)
+	require.NoError(t, err)
+}
+
+func TestCheckImmutableFieldsUpdate_UpdateImmutableBool(t *testing.T) {
+	type A struct {
+		Field1 bool
+	}
+	immutableFields := []string{"field1"}
+
+	msgReq := &A{
+		Field1: true,
+	}
+	msgUpdate := &A{
+		Field1: false,
+	}
+
+	err := checkfield.CheckImmutableFieldsUpdate(msgReq, msgUpdate, immutableFields)
+	require.EqualError(t, err, "rpc error: code = InvalidArgument desc = field path `field1` is immutable")
+}
+
+func TestCheckImmutableFieldsUpdate_UpdateImmutableStr(t *testing.T) {
+	type A struct {
+		Field1 string
+	}
+	immutableFields := []string{"field1"}
+
+	msgReq := &A{
+		Field1: "msgReq",
+	}
+	msgUpdate := &A{
+		Field1: "msgUpdate",
+	}
+
+	err := checkfield.CheckImmutableFieldsUpdate(msgReq, msgUpdate, immutableFields)
+	require.EqualError(t, err, "rpc error: code = InvalidArgument desc = field path `field1` is immutable")
+}
+
+func TestCheckImmutableFieldsUpdate_UpdateImmutableInt(t *testing.T) {
+	type A struct {
+		Field1 int
+	}
+	immutableFields := []string{"field1"}
+
+	msgReq := &A{
+		Field1: 10,
+	}
+	msgUpdate := &A{
+		Field1: 20,
+	}
+
+	err := checkfield.CheckImmutableFieldsUpdate(msgReq, msgUpdate, immutableFields)
+	require.EqualError(t, err, "rpc error: code = InvalidArgument desc = field path `field1` is immutable")
+}
+
+func TestCheckImmutableFieldsUpdate_UpdateImmutableFloat(t *testing.T) {
+	type A struct {
+		Field1 float32
+	}
+	immutableFields := []string{"field1"}
+
+	msgReq := &A{
+		Field1: 10,
+	}
+	msgUpdate := &A{
+		Field1: 20,
+	}
+
+	err := checkfield.CheckImmutableFieldsUpdate(msgReq, msgUpdate, immutableFields)
+	require.EqualError(t, err, "rpc error: code = InvalidArgument desc = field path `field1` is immutable")
+}
+
+func TestCheckImmutableFieldsUpdate_UpdateImmutableNestedStruct(t *testing.T) {
+	type A struct {
+		Field1 float32
+	}
+	type B struct {
+		Field1 *A
+	}
+
+	immutableFields := []string{"field1.field1"}
+
+	msgReq := &B{
+		Field1: &A{
+			Field1: 10,
+		},
+	}
+	msgUpdate := &B{
+		Field1: &A{
+			Field1: 20,
+		},
+	}
+
+	err := checkfield.CheckImmutableFieldsUpdate(msgReq, msgUpdate, immutableFields)
+	require.EqualError(t, err, "rpc error: code = InvalidArgument desc = field path `field1.field1` is immutable")
+}
+
+func TestCheckResourceID_Valid(t *testing.T) {
+	err := checkfield.CheckResourceID("local-user")
+	require.NoError(t, err)
+}
+
+func TestCheckResourceID_InvalidShort(t *testing.T) {
+	// 0-charactor string
+	tooShort := ""
+	err := checkfield.CheckResourceID(tooShort)
+	require.EqualError(t, err, "rpc error: code = InvalidArgument desc = `id` needs to be within ASCII-only 63 characters following RFC-1034 with a regexp (^[a-z]([a-z0-9-]{0,61}[a-z0-9])?$)")
+}
+
+func TestCheckResourceID_InvalidLong(t *testing.T) {
+
+	// 64-charactor string
+	tooLong := "abcdefghijklmnopqrstuvwxyz-ABCDEFGHIJKLMNOPQRSTUVWXYZ-0123456789"
+	err := checkfield.CheckResourceID(tooLong)
+	require.EqualError(t, err, "rpc error: code = InvalidArgument desc = `id` needs to be within ASCII-only 63 characters following RFC-1034 with a regexp (^[a-z]([a-z0-9-]{0,61}[a-z0-9])?$)")
+}
+
+func TestCheckResourceID_InvalidUUID(t *testing.T) {
+	a := "91be8b99-cd60-4081-9187-9796d01fd50b"
+	err := checkfield.CheckResourceID(a)
+	require.EqualError(t, err, "rpc error: code = InvalidArgument desc = `id` is not allowed to be a UUID")
+}
+
+func TestCheckResourceID_Invalid(t *testing.T) {
+	a := "local_user"
+	err := checkfield.CheckResourceID(a)
+	require.EqualError(t, err, "rpc error: code = InvalidArgument desc = `id` needs to be within ASCII-only 63 characters following RFC-1034 with a regexp (^[a-z]([a-z0-9-]{0,61}[a-z0-9])?$)")
+}
