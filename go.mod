fca77e54bedbf27f0dd465bc53377780066ce233
Ping-Lin Chang <ping-lin.chang@instill.tech>
2023-03-08 18:06:51 +0000

    chore(sterr): add gob errdetails struct registration

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/instill-ai/x
 
-go 1.17
+go 1.19
 
 require (
 	github.com/google/uuid v1.3.0
@@ -13,13 +13,12 @@ require (
 	google.golang.org/protobuf v1.27.1
 )
 
-require gopkg.in/yaml.v2 v2.4.0 // indirect
-
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- sterr/sterr.go
+++ sterr/sterr.go
@@ -1,6 +1,8 @@
 package sterr
 
 import (
+	"encoding/gob"
+
 	"google.golang.org/genproto/googleapis/rpc/errdetails"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -55,4 +57,22 @@ func CreateErrorResourceInfo(code codes.Code, msg string, rscType string, rscNam
 	}
 
 	return st, nil
+}
+
+// RegisterMessageTypes registers common and custom types in gob
+func RegisterMessageTypes() {
+	gob.Register(&errdetails.ErrorInfo{})
+	gob.Register(&errdetails.RetryInfo{})
+	gob.Register(&errdetails.DebugInfo{})
+	gob.Register(&errdetails.QuotaFailure{})
+	gob.Register(&errdetails.PreconditionFailure{})
+	gob.Register(&errdetails.BadRequest{})
+	gob.Register(&errdetails.RequestInfo{})
+	gob.Register(&errdetails.ResourceInfo{})
+	gob.Register(&errdetails.Help{})
+	gob.Register(&errdetails.LocalizedMessage{})
+	gob.Register(&errdetails.QuotaFailure_Violation{})
+	gob.Register(&errdetails.PreconditionFailure_Violation{})
+	gob.Register(&errdetails.BadRequest_FieldViolation{})
+	gob.Register(&errdetails.Help_Link{})
 }
