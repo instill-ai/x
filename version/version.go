b8431cc65a4d04425b3cd3e332595769d8a2b35d
Jeremy Shih <jeremyshih0628@gmail.com>
2024-11-08 13:31:05 +0800

    feat(common): git hash info func

---
+++ version/version.go
@@ -0,0 +1,14 @@
+package version
+
+import "runtime/debug"
+
+var Commit = func() string {
+	if info, ok := debug.ReadBuildInfo(); ok {
+		for _, setting := range info.Settings {
+			if setting.Key == "vcs.revision" {
+				return setting.Value
+			}
+		}
+	}
+	return ""
+}()
