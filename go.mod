dbca7ae1a0f2f1280bd85c65c738615cbaa56ffc
Juan Vallés <3977183+jvallesm@users.noreply.github.com>
2025-03-25 18:44:12 +0100

    feat(temporal): add metrics to temporal client (#45)
    Because
    
    - We want to monitor the performance of Temporal workers to scale them
    correctly.
    
    This commit
    
    - Refactors the Temporal the client option constructor. Check
    https://github.com/instill-ai/pipeline-backend#990 for a migration
    example.
    - Adds metrics to the temporal client according to
    https://docs.temporal.io/develop/go/observability#metrics

--- go.mod
+++ go.mod
@@ -11,15 +11,20 @@ require (
 	github.com/google/uuid v1.6.0
 	github.com/iancoleman/strcase v0.2.0
 	github.com/minio/minio-go/v7 v7.0.87
+	github.com/prometheus/client_golang v1.21.1
 	github.com/stretchr/testify v1.10.0
+	github.com/uber-go/tally/v4 v4.1.16
 	go.temporal.io/sdk v1.32.1
+	go.temporal.io/sdk/contrib/tally v0.2.0
 	go.uber.org/zap v1.21.0
 	google.golang.org/genproto v0.0.0-20230410155749-daa745c078e1
 	google.golang.org/grpc v1.66.0
-	google.golang.org/protobuf v1.34.2
+	google.golang.org/protobuf v1.36.1
 )
 
 require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/facebookgo/clock v0.0.0-20150410010913-600d898af40a // indirect
@@ -36,13 +41,19 @@ require (
 	github.com/kr/text v0.2.0 // indirect
 	github.com/minio/crc64nvme v1.0.1 // indirect
 	github.com/minio/md5-simd v1.1.2 // indirect
+	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/nexus-rpc/sdk-go v0.1.0 // indirect
 	github.com/pborman/uuid v1.2.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/prometheus/client_model v0.6.1 // indirect
+	github.com/prometheus/common v0.62.0 // indirect
+	github.com/prometheus/procfs v0.15.1 // indirect
 	github.com/robfig/cron v1.2.0 // indirect
 	github.com/rogpeppe/go-internal v1.11.0 // indirect
 	github.com/rs/xid v1.6.0 // indirect
 	github.com/stretchr/objx v0.5.2 // indirect
+	github.com/twmb/murmur3 v1.1.5 // indirect
 	go.temporal.io/api v1.44.1 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
@@ -53,6 +64,5 @@ require (
 	golang.org/x/sys v0.30.0 // indirect
 	golang.org/x/text v0.22.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- temporal/clientoptions.go
+++ temporal/clientoptions.go
@@ -5,63 +5,127 @@ import (
 	"crypto/x509"
 	"fmt"
 	"os"
+	"time"
 
+	"github.com/uber-go/tally/v4"
+	"github.com/uber-go/tally/v4/prometheus"
 	"go.temporal.io/sdk/client"
+	"go.uber.org/zap"
+
+	prom "github.com/prometheus/client_golang/prometheus"
+	sdktally "go.temporal.io/sdk/contrib/tally"
 
 	"github.com/instill-ai/x/zapadapter"
 )
 
-// GetClientOption takes
-// hostport: host:port of the Temporal server
-// namespace: namespace to use
-// logger: a zap logger
-func GetClientOption(hostport string, namespace string, logger *zapadapter.ZapAdapter) (client.Options, error) {
-	return client.Options{
-		HostPort:  hostport,
-		Namespace: namespace,
-		Logger:    logger,
-	}, nil
+// ClientConfig contains the configuration parameters for a Temporal client.
+type ClientConfig struct {
+	HostPort  string `koanf:"hostport"`
+	Namespace string `koanf:"namespace"`
+	Retention string `koanf:"retention"`
+
+	MetricsPort int `koanf:"metricsport"` // Listener address for the Temporal metrics to be scraped
+
+	// Secure communication config.
+	ServerName   string `koanf:"servername"`   // Server name to use for verifying the server certificate and as metrics prefix
+	ServerRootCA string `koanf:"serverrootca"` // Path to the server root CA certificate
+	ClientCert   string `koanf:"clientcert"`   // Path to the client certificate
+	ClientKey    string `koanf:"clientkey"`    // Path to the client
+
+	// InsecureSkipVerify controls whether a client verifies the server's
+	// certificate chain and host name. If InsecureSkipVerify is true, crypto/tls
+	// accepts any certificate presented by the server and any host name in that
+	// certificate. In this mode, TLS is susceptible to machine-in-the-middle
+	// attacks unless custom verification is used. This should be used only for
+	// testing or in combination with VerifyConnection or VerifyPeerCertificate.
+	InsecureSkipVerify bool `koanf:"insecureskipverify"`
 }
 
-// GetTLSClientOption takes
-// hostport: host:port of the Temporal server
-// namespace: namespace to use
-// logger: a zap logger
-// serverRootCACert: path to the server's root CA cert
-// clientCert: path to the client's cert
-// clientKey: path to the client's key
-// serverName: server name to use for verifying the server's certificate
-// insecureSkipVerify: skip verification of the server's certificate and host name
-func GetTLSClientOption(hostport string, namespace string, logger *zapadapter.ZapAdapter, serverRootCACert string, clientCert string, clientKey string, serverName string, insecureSkipVerify bool) (client.Options, error) {
-	// Load client cert
-	cert, err := tls.LoadX509KeyPair(clientCert, clientKey)
-	if err != nil {
-		return client.Options{}, fmt.Errorf("failed loading client cert and key: %w", err)
+// ClientOptions returns a Temporal client configuration based on the provided
+// configuration.
+func ClientOptions(cfg ClientConfig, log *zap.Logger) (client.Options, error) {
+	opts := client.Options{
+		HostPort:  cfg.HostPort,
+		Namespace: cfg.Namespace,
+		Logger:    zapadapter.NewZapAdapter(log),
 	}
 
-	// Load server CA if given
-	var serverCAPool *x509.CertPool
-	if serverRootCACert != "" {
-		serverCAPool = x509.NewCertPool()
-		b, err := os.ReadFile(serverRootCACert)
+	if cfg.ServerRootCA != "" && cfg.ClientCert != "" && cfg.ClientKey != "" {
+		connOpts, err := getTLSConnOptions(cfg)
 		if err != nil {
-			return client.Options{}, fmt.Errorf("failed reading server CA: %w", err)
-		} else if !serverCAPool.AppendCertsFromPEM(b) {
-			return client.Options{}, fmt.Errorf("server CA PEM file invalid")
+			return opts, fmt.Errorf("getting Temporal options: %w", err)
 		}
+
+		opts.ConnectionOptions = connOpts
 	}
 
-	return client.Options{
-		HostPort:  hostport,
-		Namespace: namespace,
-		Logger:    logger,
-		ConnectionOptions: client.ConnectionOptions{
-			TLS: &tls.Config{
-				Certificates:       []tls.Certificate{cert},
-				RootCAs:            serverCAPool,
-				ServerName:         serverName,
-				InsecureSkipVerify: true,
+	if cfg.MetricsPort != 0 {
+		ps, err := newPrometheusScope(prometheus.Configuration{
+			ListenAddress: fmt.Sprintf("0.0.0.0:%d", cfg.MetricsPort),
+			TimerType:     "histogram",
+		}, log)
+		if err != nil {
+			return opts, fmt.Errorf("creating Prometheus metrics scope: %w", err)
+		}
+
+		opts.MetricsHandler = sdktally.NewMetricsHandler(ps)
+	}
+
+	return opts, nil
+}
+
+func getTLSConnOptions(cfg ClientConfig) (opts client.ConnectionOptions, err error) {
+	cert, err := tls.LoadX509KeyPair(cfg.ClientCert, cfg.ClientKey)
+	if err != nil {
+		return opts, fmt.Errorf("loading client cert and key: %w", err)
+	}
+
+	opts.TLS = &tls.Config{
+		Certificates:       []tls.Certificate{cert},
+		ServerName:         cfg.ServerName,
+		InsecureSkipVerify: cfg.InsecureSkipVerify,
+	}
+
+	if cfg.ServerRootCA != "" {
+		serverCAPool := x509.NewCertPool()
+		b, err := os.ReadFile(cfg.ServerRootCA)
+		if err != nil {
+			return opts, fmt.Errorf("failed reading server CA: %w", err)
+		}
+
+		if !serverCAPool.AppendCertsFromPEM(b) {
+			return opts, fmt.Errorf("server CA PEM file invalid")
+		}
+
+		opts.TLS.RootCAs = serverCAPool
+	}
+
+	return opts, nil
+}
+
+func newPrometheusScope(c prometheus.Configuration, log *zap.Logger) (tally.Scope, error) {
+	reporter, err := c.NewReporter(
+		prometheus.ConfigurationOptions{
+			Registry: prom.NewRegistry(),
+			OnError: func(err error) {
+				log.Error("Error in prometheus reporter", zap.Error(err))
 			},
 		},
-	}, nil
+	)
+	if err != nil {
+		return nil, fmt.Errorf("creating prometheus reporter: %w", err)
+	}
+
+	// By convention, metrics will use snake_case.
+	scopeOpts := tally.ScopeOptions{
+		CachedReporter:  reporter,
+		Separator:       prometheus.DefaultSeparator,
+		SanitizeOptions: &sdktally.PrometheusSanitizeOptions,
+		// A prefix can be set here, but metrics are already grouped by namespace.
+	}
+	scope, _ := tally.NewRootScope(scopeOpts, time.Second)
+	scope = sdktally.NewPrometheusNamingScope(scope)
+
+	log.Info("Prometheus metrics scope created")
+	return scope, nil
 }
