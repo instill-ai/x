55c0b3cae3281f0aeda380cea58defb228f7dbec
Juan Vallés <jvalles@instill.tech>
2025-05-22 09:21:53 +0200

    chore(constant): add common Instill headers

--- constant/constant.go
+++ constant/constant.go
@@ -1,9 +1,23 @@
 package constant
 
 const (
-	HeaderUserUIDKey      = "Instill-User-Uid"
+	// HeaderUserUIDKey is the context key for the authenticated user.
+	HeaderUserUIDKey = "Instill-User-Uid"
+	// HeaderRequesterUIDKey is the context key for the requester. An
+	// authenticated user can use different namespaces (e.g. an organization
+	// they belong to) to make requests, as long as they have permissions.
 	HeaderRequesterUIDKey = "Instill-Requester-Uid"
-	HeaderUserAgent       = "Instill-User-Agent"
+	// HeaderVisitorUIDKey is the context key for the visitor UID when requests
+	// are made without authentication.
+	HeaderVisitorUIDKey = "Instill-Visitor-Uid"
+	// HeaderAuthTypeKey is the context key the authentication type (user or
+	// visitor).
+	HeaderAuthTypeKey = "Instill-Auth-Type"
+	// HeaderUserAgentKey identifies the agent that's making a request. Its
+	// accepted values are the string values of
+	// github.com/instill-ai/protogen-go/common/run/v1alpha.RunSource.
+	HeaderUserAgentKey = "Instill-User-Agent"
 
+	// ContentTypeJSON is the value for the JSON content type.
 	ContentTypeJSON = "application/json"
 )
